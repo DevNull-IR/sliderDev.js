f11926e2fe09ecd9153e9717ade885f17b618bb2
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-05-17 22:35:02 +0430

    Update slider.js

--- src/slider.js
+++ src/slider.js
@@ -1,41 +1,55 @@
 function makeSlider(SliderName, inId, imgs = [], stylesCss = ''){
-    // console.log(imgs);
     let images = '';
     if(imgs.length > 0){
+        document.write(`<style id="stylesheet${SliderName}">
+            .img${SliderName}:nth-child(1){display: block;}
+        </style>`);
         imgs.forEach(function(items){
-                images += `\n <img class="img`+SliderName+`" src="`+items+`" alt="">`;
+                images += `\n <img class="img${SliderName}" src="${items}" alt="">`;
         });
         document.getElementById(inId).innerHTML = `
-        <style id="stylesheet`+ SliderName +`">
-            .img`+ SliderName +`:nth-child(1){display: block;}
-        </style>
+        
         <style>
-            section#slider`+ SliderName +`{
+            section#slider${SliderName}{
             width: 500px;
             height: 300px;
             border-radius: 20px;
             display: flex;
+            /* position: absolute; */
         }
-        .img`+ SliderName +`{
-            width: 500px;
-            height: 300px;
-            display: none;
-            // cursor: grab;
-        }
+        .img${SliderName}{
+                    width: 500px;
+                    height: 300px;
+                    display: none;
+                    cursor: grab;
+                    animation-name:  ${SliderName};
+                    animation-duration: 1s;
+                    // opacity: 0%;
+                    /* position: absolute; */
+                }
+                @keyframes ${SliderName}{
+                    from{
+                        opacity: 30%;
+                    }
+                    to{
+                        opacity: 100%;
+                    }
+                }
         `+stylesCss+`
         </style>
-        <section id="slider`+ SliderName +`">
+        <section id="slider${SliderName}">
         `+images+`
         </section>
-        <input type="button" value="next" id="nextSlider`+SliderName+`">
-        <input type="button" value="back" id="BackSlider`+SliderName+`">
-        <input type="hidden" value="1" id="sliderResult`+SliderName+`">
-        <input type="hidden" value="o" id="sliderResultSet`+SliderName+`">`;
-        let nextSlider = document.getElementById('nextSlider'+SliderName);
-        let BackSlider = document.getElementById('BackSlider'+SliderName);
-        let sliderResult = document.getElementById('sliderResult'+SliderName);
-        let styles = document.getElementById('stylesheet'+SliderName);
-        let sliderResultSet = document.getElementById('sliderResultSet'+SliderName).value;
+        <input type="button" value="next" id="nextSlider${SliderName}">
+        <input type="button" value="back" id="BackSlider${SliderName}">
+        <input type="hidden" value="1" id="sliderResult${SliderName}">
+        <input type="hidden" value="o" id="sliderResultSet${SliderName}">`;
+        let nextSlider = document.getElementById(`nextSlider${SliderName}`);
+        let BackSlider = document.getElementById(`BackSlider${SliderName}`);
+        let sliderResult = document.getElementById(`sliderResult${SliderName}`);
+        let styles = document.getElementById(`stylesheet${SliderName}`);
+        let sliderResultSet = document.getElementById(`sliderResultSet${SliderName}`).value;
+        console.log(document.getElementsByClassName('img'+SliderName).namedItem['img']);
         function next() {
             styles.innerHTML = ".img"+SliderName+":nth-child("+sliderResult.value+"){display: block;}";
         }
